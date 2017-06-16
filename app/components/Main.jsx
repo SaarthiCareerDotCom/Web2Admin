8cb4565b0942dd5df48236208e1449d771ca7084
shubham41995 <shubhammundhra1995@gmail.com>
2017-06-17 04:29:13 +0530

    Modified Course Page

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -4,12 +4,8 @@ var Nav = require('Nav');
 var Main = (props) => {
   return (
     <div>
-      <Nav/>
-      <div className="row">
-        <div className="columns medium-6 large-4 small-centered">
-          {props.children}
-        </div>
-      </div>
+      <Nav />
+      {props.children}
     </div>
   );
 }
